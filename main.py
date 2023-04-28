3c1849a40157eb49629a7641692f4c2beb445270
quweent <131997510+quweent@users.noreply.github.com>
2023-04-28 15:42:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-import schedule
 import time
+import pyrogram
+from pyrogram import Client
 
-from hikka import Hikka
-from hikka.decorators import register
+# Создаем клиента API
+app = Client("my_bot")
 
-bot = Hikka()
+# Обработчик команды "/farmdick"
+@app.on_message(pyrogram.filters.command("farmdick"))
+def send_dick(client, message):
+    # Отправляем сообщение в группу
+    client.send_message(chat_id="GROUP_CHAT_ID", text="/dick@dickupbot")
 
-@register(events.NewMessage(pattern='/farmdick'))
-async def start_schedule(event):
-    # Запустить расписание, когда получена команда /farmdick
-    schedule_pick()
-    await event.reply('Scheduled /dick@dickupbot every hour!')
+# Запускаем бота
+app.run()
 
-def job():
-    # Функция для выполнения задания
-    bot.send_message('dickupbot', '/dick')
-
-def schedule_pick():
-    # Запустить задание каждый час
-    schedule.every().hour.do(job)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-bot.run()
+# Отправляем сообщение каждый час
+while True:
+    with app:
+        app.send_message(chat_id="GROUP_CHAT_ID", text="/dick@dickupbot")
+    time.sleep(3600) # ожидаем 1 час
